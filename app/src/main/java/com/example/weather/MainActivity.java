84786be8466ca82ea06fc5e301c9e381a85cb6ac
priputin <v-priputin@yandex.ru>
2020-07-29 18:36:22 +0300

    Работа над ошибками ДЗ

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,6 +15,9 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Main Weather Info Activity
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView cityView;
@@ -20,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView feelsLikeView;
     private TextView cloudinessView;
     private ImageView weatherView;
-    private final int DATE_TIME_UPDATE_MS = 1000;
-    private Handler handlerDateTime;
-    private Runnable dateTimeUpdater;
 
+    private BroadcastReceiver dateTimeChangedReceiver;
+
+    /**
+     * Activity on create stuff
+     * @param savedInstanceState - activity saved instance state
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,48 +40,67 @@ public class MainActivity extends AppCompatActivity {
 
         findViews();
         updateLocation();
-        updateDate();
+
         updateTemp();
         updateFeelsLikeTempView();
         updateCloudinessView();
         updateWeatherView();
+    }
+
+    /**
+     * Activity on pause stuff
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableDateTimeUpdate();
+    }
+
+    /**
+     * Activity on resume stuff
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
         initDateTimeUpdate();
     }
 
-    @Override
-    protected void onPause() {
-        handlerDateTime.removeCallbacks(dateTimeUpdater);
-        super.onPause();
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        updateDate();
-        handlerDateTime.postDelayed(dateTimeUpdater, DATE_TIME_UPDATE_MS);
-    }
-
+    /**
+     * Enable auto update date-time view
+     */
     private void initDateTimeUpdate() {
-        if ( handlerDateTime == null) {
-            handlerDateTime = new Handler();
-        }
-        if ( dateTimeUpdater == null ) {
-            dateTimeUpdater = new Runnable() {
-                @Override
-                public void run() {
-                    updateDate();
-                    handlerDateTime.postDelayed(this, DATE_TIME_UPDATE_MS);
-                }
-            };
-        }
-        handlerDateTime.removeCallbacks(dateTimeUpdater);
-        handlerDateTime.postDelayed(dateTimeUpdater, DATE_TIME_UPDATE_MS);
+        updateDate();
+        dateTimeChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                updateDate();
+            }
+        };
+        IntentFilter dateTimeUpdateFilter = new IntentFilter();
+        dateTimeUpdateFilter.addAction(Intent.ACTION_TIME_CHANGED); //manually time set
+        dateTimeUpdateFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        dateTimeUpdateFilter.addAction(Intent.ACTION_DATE_CHANGED); //manually date set
+        dateTimeUpdateFilter.addAction(Intent.ACTION_TIME_TICK);    //every 1 minute
+        registerReceiver(dateTimeChangedReceiver, dateTimeUpdateFilter);
     }
 
+    /**
+     * Disable auto update date-time view
+     */
+    private void disableDateTimeUpdate() {
+       unregisterReceiver(dateTimeChangedReceiver);
+    }
+
+    /**
+     * Update location view
+     */
     private void updateLocation() {
         cityView.setText("Moscow");
     }
 
+    /**
+     * Update date-time status view
+     */
     private void updateDate() {
         Date currentDate = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("E, dd MMM HH:mm", Locale.getDefault());
@@ -80,25 +108,40 @@ public class MainActivity extends AppCompatActivity {
         dateTimeView.setText(dateTime);
     }
 
+    /**
+     * Update temperature value in view
+     */
     private void updateTemp() {
         int temp = 13; //градусов цельсия
         String currentTemperature = Integer.toString(temp) + getString(R.string.temperature_unit);
         temperatureView.setText(currentTemperature);
     }
 
+    /**
+     * Update weather feelslike status
+     */
     private void updateFeelsLikeTempView() {
         int feelsLikeTemp = 11;
         feelsLikeView.setText(getString(R.string.feels_like) + " " + Integer.toString(feelsLikeTemp) + getString(R.string.temperature_unit));
     }
 
+    /**
+     * Update weather cloudiness status
+     */
     private void updateCloudinessView() {
         cloudinessView.setText(getString(R.string.cloudly));
     }
 
+    /**
+     * Update Weather status image
+     */
     private void updateWeatherView() {
         weatherView.setImageResource(R.mipmap.ic_cloudly);
     }
 
+    /**
+     * Find activity views
+     */
     private void findViews() {
         cityView = findViewById( R.id.cityView );
         dateTimeView = findViewById( R.id.dateView );
